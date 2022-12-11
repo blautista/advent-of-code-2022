066392e6744424f366e7b72a1196fc363002778b
blautista <bautistaromanazzi@gmail.com>
2022-12-11 20:00:27 -0300

    refactor solution printing

--- 01/index.ts
+++ 01/index.ts
@@ -1,4 +1,4 @@
-import { readInput } from "../utils.js";
+import { printSolutions, readInput } from "../utils.js";
 import { EOL } from "node:os";
 
 const input = readInput(import.meta.url);
@@ -6,17 +6,15 @@ const input = readInput(import.meta.url);
 const getSums = (input: string): number[] => {
   const elvesCalories = input.split(`${EOL}${EOL}`);
 
-  console.log(elvesCalories);
   return elvesCalories.map((cals) => {
     return cals.split(EOL).reduce((prev, curr) => prev + Number(curr), 0);
   });
 };
 
-console.log(`Part one: ${Math.max(...getSums(input))}`);
-
-console.log(
-  `Part two: ${getSums(input)
+printSolutions(
+  Math.max(...getSums(input)),
+  getSums(input)
     .sort((a, b) => b - a)
     .slice(0, 3)
-    .reduce((prev, curr) => prev + curr, 0)}`
+    .reduce((prev, curr) => prev + curr, 0)
 );
--- utils.ts
+++ utils.ts
@@ -3,3 +3,10 @@ import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 export const readInput = (url: string) => readFileSync(path.join(dirname(fileURLToPath(url)), "input.txt"), "utf8");
+
+export const printSolutions = (first: number, second: number) => {
+  console.log(`Part one: ${first}`);
+  console.log(`Part two: ${second}`);
+};
+
+export const sumArray = (numbers: []) => numbers.reduce((a, b) => a + b, 0);
