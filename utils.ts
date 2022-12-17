06d2a0445b5a36a8998bc86c79416e58da54e000
blautista <bautistaromanazzi@gmail.com>
2022-12-17 01:02:09 -0300

    add support for string arrays in printSolutions

--- utils.ts
+++ utils.ts
@@ -5,9 +5,9 @@ import { fileURLToPath } from "node:url";
 export const readInput = (url: string, test?: boolean) =>
   readFileSync(path.join(dirname(fileURLToPath(url)), test ? "test_input.txt" : "input.txt"), "utf8");
 
-export const printSolutions = (first: number | string, second: number | string) => {
-  console.log(`Part one: ${first}`);
-  console.log(`Part two: ${second}`);
+export const printSolutions = (first: number | string | string[], second: number | string | string[]) => {
+  console.log("Part one:", first);
+  console.log("Part two:", second);
 };
 
 export const sumArray = (numbers: number[]) => numbers.reduce((a, b) => a + b, 0);
