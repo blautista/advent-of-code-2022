34fe32b45691256d6ed78520572e8eded2d6a6ae
blautista <bautistaromanazzi@gmail.com>
2022-12-23 22:26:23 -0300

    add total run time to printSolutions

--- utils.ts
+++ utils.ts
@@ -2,12 +2,15 @@ import { readFileSync } from "node:fs";
 import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
+const start = performance.now();
+
 export const readInput = (url: string, test?: boolean) =>
   readFileSync(path.join(dirname(fileURLToPath(url)), test ? "test_input.txt" : "input.txt"), "utf8");
 
 export const printSolutions = (first: number | string | string[], second: number | string | string[]) => {
   console.log("Part one:", first);
   console.log("Part two:", second);
+  console.log(`Total run time: ${((performance.now() - start) / 1000).toFixed(3)}s`);
 };
 
 export const sumArray = (numbers: number[]) => numbers.reduce((a, b) => a + b, 0);
